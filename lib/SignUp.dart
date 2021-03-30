0ccc70e64ba0da95d9a4922a1e1dbb743ddfb53c
safad <safadeldoul@gmail.com>
2021-03-30 01:10:32 +0100

    fixed sign up ui

--- lib/SignUp.dart
+++ lib/SignUp.dart
@@ -4,45 +4,50 @@ class SignUp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-      Center(
-        child: Padding(
-          padding: const EdgeInsets.all(50.0),
-          child: Text(
-            "Let's Get Started!",
-            style: TextStyle(
-                fontWeight: FontWeight.w600, color: Colors.black, fontSize: 25),
+        body: Container(
+      decoration: BoxDecoration(color: Colors.white),
+      child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+        Center(
+          child: Padding(
+            padding: const EdgeInsets.all(50.0),
+            child: Text(
+              "Let's Get Started!",
+              style: TextStyle(
+                  fontWeight: FontWeight.w600,
+                  color: Colors.black,
+                  fontSize: 25),
+            ),
           ),
         ),
-      ),
-      Padding(
-        padding: const EdgeInsets.all(15.0),
-        child: TextField(
-          decoration: new InputDecoration(
-            suffixIcon:
-                IconButton(onPressed: () {}, icon: Icon(Icons.person_outline)),
-            contentPadding: EdgeInsets.all(8),
-            labelText: 'Email',
-            border: new OutlineInputBorder(
-                borderRadius: BorderRadius.circular(30),
-                borderSide: new BorderSide(color: Colors.teal)),
+        Padding(
+          padding: const EdgeInsets.all(15.0),
+          child: TextField(
+            decoration: new InputDecoration(
+              suffixIcon: IconButton(
+                  onPressed: () {}, icon: Icon(Icons.person_outline)),
+              contentPadding: EdgeInsets.all(8),
+              labelText: 'Email',
+              border: new OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(30),
+                  borderSide: new BorderSide(color: Colors.teal)),
+            ),
           ),
         ),
-      ),
-      Padding(
-        padding: const EdgeInsets.all(15.0),
-        child: TextField(
-          decoration: new InputDecoration(
-            suffixIcon:
-                IconButton(onPressed: () {}, icon: Icon(Icons.lock_outline)),
-            contentPadding: EdgeInsets.all(8),
-            labelText: 'Password',
-            border: new OutlineInputBorder(
-                borderRadius: BorderRadius.circular(30),
-                borderSide: new BorderSide(color: Colors.teal)),
+        Padding(
+          padding: const EdgeInsets.all(15.0),
+          child: TextField(
+            decoration: new InputDecoration(
+              suffixIcon:
+                  IconButton(onPressed: () {}, icon: Icon(Icons.lock_outline)),
+              contentPadding: EdgeInsets.all(8),
+              labelText: 'Password',
+              border: new OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(30),
+                  borderSide: new BorderSide(color: Colors.teal)),
+            ),
           ),
         ),
-      ),
-    ]));
+      ]),
+    ));
   }
 }
